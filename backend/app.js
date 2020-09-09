81ba67d1cf394bbd947fc59dd99a62ea280e45b6
Ankit Singh <ankit.15bcs1181@gmail.com>
2020-09-10 00:49:59 +0530

    Now we can send a post data to the server, however we are still not using any database to permanently store the data at the backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // the following code block will allow other domains to access resources at our server
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     next();
 });
 
-app.use('/api/posts',(req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
+});
+
+app.get('/api/posts',(req, res, next) => {
     
     const posts = [
         {id: '1', title: 'Server post title1', content: 'this is coming directly from server'},
