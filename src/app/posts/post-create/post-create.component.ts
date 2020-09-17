9e2a0564583e1d45f866de4507eb78baa2691e23
Ankit Singh <ankit.15bcs1181@gmail.com>
2020-09-17 22:35:15 +0530

    Image file validator added now we can't select file other than Image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
+
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
@@ -28,7 +30,7 @@ export class PostCreateComponent implements OnInit{
     ngOnInit() {
         this.form = new FormGroup({
             title:new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-            content: new FormControl(null, {validators: [Validators.required]}),image: new FormControl(null, {validators: [Validators.required]})
+            content: new FormControl(null, {validators: [Validators.required]}),image: new FormControl(null, {validators: [Validators.required],asyncValidators: [mimeType]})
         });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if(paramMap.has('postId')){
