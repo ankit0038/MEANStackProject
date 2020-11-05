d447ca55975f78e98a8dec3b0a3eb4506442d5a1
Ankit Singh <ankit.15bcs1181@gmail.com>
2020-11-06 00:01:16 +0530

    signup and login functionality added

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const path = require("path");
 
 const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
 const app = express();
 
 mongoose.connect("mongodb+srv://max:jJtrC3G7jigZax3l@cluster0.7yd6l.mongodb.net/posts?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology:true }).then(() => {
@@ -26,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 
 module.exports = app;
